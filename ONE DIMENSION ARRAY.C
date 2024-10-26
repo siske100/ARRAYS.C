51797075a34eb0666ed87d37c64cddfdeb5ee510
siske100 <166230659+siske100@users.noreply.github.com>
2024-10-26 06:58:32 -0700

    Update and rename README.md to ONE DIMENSION ARRAY.C

---
+++ ONE DIMENSION ARRAY.C
@@ -0,0 +1,12 @@
+//one-dimension array
+#include<stdio.h> 
+int main(){
+	int i;
+	float marks [5]={50.1,60.1,70.1,80.1,90.1};
+	
+	for(i=0;i<5;i++){
+		printf("%f\n",marks[i]);
+		
+	}
+	return 0;
+}
